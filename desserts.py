df40bc358688ebb4c4b167ce66537e0f687b8f31
Stephanie Resis <sresis@umich.edu>
2020-07-20 19:47:06 +0000

    added Class functionality

--- desserts.py
+++ desserts.py
@@ -6,9 +6,45 @@ class Cupcake:
     cache = {}
 
     def __init__(self, name, flavor, price):
-      self.name = names
+      cache = {}
+      self.name = name
       self.flavor = flavor
       self.price = price
+      self.qty = 0
+
+      self.cache[name] = self
+
+    def add_stock(self, amount):
+      self.qty = self.qty + amount
+
+    def sell(self, amount):
+
+      #if self.qty > 0 and self.qty < amount:
+       # print(f'Not enough cupcakes. there are {self.qty} left.')
+
+      while self.qty > 0 and amount > 0:
+        self.qty = self.qty - 1
+        amount +- 1
+      if self.qty < 0:
+        print('Sorry, these cupcakes are sold out')
+
+
+
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+
+      for ingredient in ingredients:
+        ingredient[1] = ingredient[1] * amount
+
+      return ingredients
+
+    @classmethod
+    def get(cls, name):
+      if self.name not in Class.cache:
+        print(f"Sorry, that cupcake doesn't exist")
+
+
+
 
     def __repr__(self):
         """Human-readable printout for debugging."""
